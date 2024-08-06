907aa9f4b3f80d9232b8faf88d79f8e5b1d34e50
oleksandr-ishchuk <alexmway@mail.ru>
2024-08-06 18:25:36 +0200

    add styles for exercise

--- js/views/WorkingField.js
+++ js/views/WorkingField.js
@@ -27,12 +27,16 @@ class WorkingField
         const statistic = new Statistic();
         statistic.init(localStorage.getItem(levelForStaticticOk),localStorage.getItem(levelForStaticticError));
 
+        // Настроить переводчик
+        let fieldsetStart = '<fieldset class="fieldSetWorkField"><legend>Вопрос с вариантами ответа</legend>';
+        
         let buttonOption = [];
         buttonOption[0] = `<div class='row'><div class='col-12 btn'><button class="btn btn-secondary" type='button' id='option1'> ${this.option1} </button></div></div>`;
         buttonOption[1] = `<div class='row'><div class='col-12 btn'><button class="btn btn-secondary" type='button' id='option2'> ${this.option2} </button></div></div>`;
         buttonOption[2] = `<div class='row'><div class='col-12 btn'><button class="btn btn-secondary" type='button' id='option3'> ${this.option3} </button></div></div>`;
         buttonOption[3] = `<div class='row'><div class='col-12 btn'><button class="btn btn-secondary" type='button' id='option4'> ${this.option4} </button></div></div>`;
         
+        const fieldsetFinish = "</fieldset>";
         const buttonQuestion = `<br><div class='row'><div class='col-12 btn'><button class="btn btn-info" type='button' id='question'> ${this.question} </button></div></div><br>`;
 
         let translateFromArray = '';
@@ -48,6 +52,13 @@ class WorkingField
     
         buttonOption = shuffleArray(buttonOption);
 
-        return buttonQuestion+translate+buttonOption[0]+buttonOption[1]+buttonOption[2]+buttonOption[3];
+        return fieldsetStart+
+                 buttonQuestion+
+                   translate+
+                     buttonOption[0]+
+                       buttonOption[1]+
+                         buttonOption[2]+
+                           buttonOption[3]+
+                             fieldsetFinish;
     }
 }
