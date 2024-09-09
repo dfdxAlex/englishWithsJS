bfe0537560db057dad0cddd387e2e2369576c769
oleksandr-ishchuk <alexmway@mail.ru>
2024-09-09 20:18:35 +0200

    cleaning

--- js/views/MenuRight.js
+++ js/views/MenuRight.js
@@ -12,34 +12,6 @@ class MenuRight
         this.seedMenu = 'burger';
     }
     
-    // menu()
-    // {
-    //     const rez = this.nameButtons.map((element, index) => {
-    //         return `<button class="btn btn-info" type="button" id="level${index+1}">${element}</button><br></br>`;
-    //     }).join('');
-
-    //    return rez;
-    // }
-
-    // menuDropdown()
-    // {
-    //     let rez = '<div class="btn-group" role="group" aria-label="Button group with nested dropdown">';
-    //     rez+='<div class="btn-group" role="group">';
-    //     rez+='<button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">';
-    //     rez+=this.nameMenu;
-    //     rez+='</button>';
-    //     rez+='<ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">';
-        
-    //     rez+= this.nameButtons.map((element, index) => {
-    //         return `<li><a class="dropdown-item" id="level${index+1}">${element}</a></li>`;
-    //     }).join('');
-
-    //     rez+='</ul>';
-    //     rez+='</div></div>';
-
-    //     return rez;
-    // }
-
     menuDropdown2()
     {
         let rez = '<div class="btn-group" role="group" aria-label="Button group with nested dropdown">';
