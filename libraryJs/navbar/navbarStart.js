707d47463506b0ad31ce04f14c8f2fc311ffa52d
oleksandr-ishchuk <alexmway@mail.ru>
2024-09-22 19:23:57 +0200

    help for nano

--- libraryJs/navbar/navbarStart.js
+++ libraryJs/navbar/navbarStart.js
@@ -1,3 +1,7 @@
-import {PropertysNavbar} from "./PropertysNavbar.js"
 
+// Указать ИД для размещения меню.
+PropertysNavbar.idMenu = 'burger';
+PropertysNavbar.logo_images = 'src/logo.png';
+PropertysNavbar.setMenu();
 
+console.log(PropertysNavbar.menu);
