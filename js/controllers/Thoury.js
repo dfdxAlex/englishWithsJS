88f3b98708cbfa073802a0e852ecf75fb4b7aa72
oleksandr-ishchuk <alexmway@mail.ru>
2024-11-02 00:55:23 +0100

    refaktoring system for help

--- js/controllers/Thoury.js
+++ js/controllers/Thoury.js
@@ -34,7 +34,7 @@ static handleThoury(level)
      <button type="button" class="btn-close" aria-label="Close" style="position: absolute; top: 10px; right: 10px;"  onclick="document.getElementById('exercise').style.display='none'"></button>
       <div class="card-body">
         <h5 class="card-title">${this.headerThoury()}</h5>
-        <p class="card-text">${this.bodyThoury()}</p>
+        <p class="card-text"style="font-size: 12px">${this.bodyThoury()}</p>
       </div>
     </div>
     `;
