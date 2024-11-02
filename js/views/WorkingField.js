70fe5952ac961c3001a370b447e99702b5c5c8d4
oleksandr-ishchuk <alexmway@mail.ru>
2024-11-02 02:51:31 +0100

    refaktoring workfield

--- js/views/WorkingField.js
+++ js/views/WorkingField.js
@@ -1,54 +1,110 @@
-// класс получает данные для создания рабочей формы и создает её
+//класс получает данные для создания рабочей формы и создает её
 
-class WorkingField
-{
-    init(arrayBD)
-    {
-      if (arrayBD !== undefined && arrayBD.length == 8)
-      [this.question, 
-        this.option1,
-        this.option2, 
-        this.option3, 
-        this.option4,
-        this.translateRu,
-        this.translateUa,
-        this.translatePl,
-       ] = arrayBD;
+// class WorkingField
+// {
+//     init(arrayBD)
+//     {
+//       if (arrayBD !== undefined && arrayBD.length == 8)
+//       [this.question, 
+//         this.option1,
+//         this.option2, 
+//         this.option3, 
+//         this.option4,
+//         this.translateRu,
+//         this.translateUa,
+//         this.translatePl,
+//        ] = arrayBD;
 
-        let levelForStaticticOk = 'level'+localStorage.getItem('level')+'_Ok';
-        let levelForStaticticError = 'level'+localStorage.getItem('level')+'_Error';
+//         let levelForStaticticOk = 'level'+localStorage.getItem('level')+'_Ok';
+//         let levelForStaticticError = 'level'+localStorage.getItem('level')+'_Error';
 
-        const statistic = FactoryRegistr.getObject('Statistic');
-        statistic.init(localStorage.getItem(levelForStaticticOk),localStorage.getItem(levelForStaticticError));
+//         const statistic = FactoryRegistr.getObject('Statistic');
+//         statistic.init(localStorage.getItem(levelForStaticticOk),localStorage.getItem(levelForStaticticError));
 
-        // Настроить переводчик
-        const transL = FactoryRegistr.getObject("LanguageController");
-        let fieldsetStart = `<fieldset class="fieldSetWorkField"><legend> ${transL.translate('Вопрос с вариантами ответа')} </legend>`;
+//         // Настроить переводчик
+//         const transL = FactoryRegistr.getObject("LanguageController");
         
-        let buttonOption = [];
-        buttonOption[0] = `<div class='row'><div class='col-12 btn'><button class="btn btn-secondary" type='button' id='option1'> ${this.option1} </button></div></div>`;
-        buttonOption[1] = `<div class='row'><div class='col-12 btn'><button class="btn btn-secondary" type='button' id='option2'> ${this.option2} </button></div></div>`;
-        buttonOption[2] = `<div class='row'><div class='col-12 btn'><button class="btn btn-secondary" type='button' id='option3'> ${this.option3} </button></div></div>`;
-        buttonOption[3] = `<div class='row'><div class='col-12 btn'><button class="btn btn-secondary" type='button' id='option4'> ${this.option4} </button></div></div>`;
+//         let fieldsetStart = `<fieldset class="fieldSetWorkField"><legend> ${transL.translate('Вопрос с вариантами ответа')} </legend>`;
         
-        const fieldsetFinish = "</fieldset>";
-        const buttonQuestion = `<br><div class='row'><div class='col-12 btn'><button class="btn btn-info" type='button' id='question'> ${this.question} </button></div></div><br>`;
+//         let buttonOption = [];
+//         buttonOption[0] = `<div class='row'><div class='col-12 btn'><button class="btn btn-secondary" type='button' id='option1'> ${this.option1} </button></div></div>`;
+//         buttonOption[1] = `<div class='row'><div class='col-12 btn'><button class="btn btn-secondary" type='button' id='option2'> ${this.option2} </button></div></div>`;
+//         buttonOption[2] = `<div class='row'><div class='col-12 btn'><button class="btn btn-secondary" type='button' id='option3'> ${this.option3} </button></div></div>`;
+//         buttonOption[3] = `<div class='row'><div class='col-12 btn'><button class="btn btn-secondary" type='button' id='option4'> ${this.option4} </button></div></div>`;
+        
+//         const fieldsetFinish = "</fieldset>";
+//         const buttonQuestion = `<br><div class='row'><div class='col-12 btn'><button class="btn btn-info" type='button' id='question'> ${this.question} </button></div></div><br>`;
 
-        // Начальный текст для кнопки перевода вопросса
-        let translateFromArray = transL.translate('Перевести вопрос');
+//         // Начальный текст для кнопки перевода вопросса
+//         let translateFromArray = transL.translate('Перевести вопрос');
 
-        // Кнопка "Перевести вопрос"
-        const translate = `<div class='row'><div class='col-12 btn'><button class="btn btn-info" type='button' id='translate'> ${translateFromArray} </button></div></div><br><br><br>`;
+//         // Кнопка "Перевести вопрос"
+//         const translate = `<div class='row'><div class='col-12 btn'><button class="btn btn-info" type='button' id='translate'> ${translateFromArray} </button></div></div><br><br><br>`;
     
-        buttonOption = shuffleArray(buttonOption);
+//         buttonOption = shuffleArray(buttonOption);
 
-        return fieldsetStart+
-                 buttonQuestion+
-                   translate+
-                     buttonOption[0]+
-                       buttonOption[1]+
-                         buttonOption[2]+
-                           buttonOption[3]+
-                             fieldsetFinish;
-    }
+//         return fieldsetStart+
+//                  buttonQuestion+
+//                    translate+
+//                      buttonOption[0]+
+//                        buttonOption[1]+
+//                          buttonOption[2]+
+//                            buttonOption[3]+
+//                              fieldsetFinish;
+//     }
+// }
+
+class WorkingField {
+  init(arrayBD) {
+      if (arrayBD !== undefined && arrayBD.length == 8) {
+          [this.question, 
+          this.option1,
+          this.option2, 
+          this.option3, 
+          this.option4,
+          this.translateRu,
+          this.translateUa,
+          this.translatePl] = arrayBD;
+
+          let levelForStaticticOk = 'level' + localStorage.getItem('level') + '_Ok';
+          let levelForStaticticError = 'level' + localStorage.getItem('level') + '_Error';
+
+          const statistic = FactoryRegistr.getObject('Statistic');
+          statistic.init(localStorage.getItem(levelForStaticticOk), localStorage.getItem(levelForStaticticError));
+
+          // Настроить переводчик
+          const transL = FactoryRegistr.getObject("LanguageController");
+
+          // Используем Card вместо fieldset
+          let cardStart = `<div class="card fieldSetWorkField" style="width: 80%; margin: auto; border: 1px solid rgba(0, 0, 0, 0.1); box-shadow: 0 8px 16px rgba(0,0,0,0.2);"><div class="card-body">`;
+          let legend = `<h5 class="card-title" id="fieldset-legend">${transL.translate('Вопрос с вариантами ответа')}</h5>`;
+
+          let buttonOption = [];
+          buttonOption[0] = `<div class='row mb-2'><div class='col-12'><button style='width: 100%; border: 1px solid rgba(0, 0, 0, 0.2); box-shadow: 0 4px 8px rgba(0,0,0,0.2);' type='button' id='option1'>${this.option1}</button></div></div>`;
+          buttonOption[1] = `<div class='row mb-2'><div class='col-12'><button style='width: 100%; border: 1px solid rgba(0, 0, 0, 0.2); box-shadow: 0 4px 8px rgba(0,0,0,0.2);' type='button' id='option2'>${this.option2}</button></div></div>`;
+          buttonOption[2] = `<div class='row mb-2'><div class='col-12'><button style='width: 100%; border: 1px solid rgba(0, 0, 0, 0.2); box-shadow: 0 4px 8px rgba(0,0,0,0.2);' type='button' id='option3'>${this.option3}</button></div></div>`;
+          buttonOption[3] = `<div class='row mb-2'><div class='col-12'><button style='width: 100%; border: 1px solid rgba(0, 0, 0, 0.2); box-shadow: 0 4px 8px rgba(0,0,0,0.2);' type='button' id='option4'>${this.option4}</button></div></div>`;
+
+          const cardFinish = "</div></div>";
+          const buttonQuestion = `<br><div class='row mb-2'><div class='col-12'><button style='width: 100%; border: 1px solid rgba(0, 0, 0, 0.2); box-shadow: 0 4px 8px rgba(0,0,0,0.2);' type='button' id='question'>${this.question}</button></div></div><br>`;
+
+          // Начальный текст для кнопки перевода вопроса
+          let translateFromArray = transL.translate('Перевести вопрос');
+
+          // Кнопка "Перевести вопрос"
+          const translate = `<div class='row mb-2'><div class='col-12'><button style='width: 100%; border: 1px solid rgba(0, 0, 0, 0.2); box-shadow: 0 4px 8px rgba(0,0,0,0.2);' type='button' id='translate'>${translateFromArray}</button></div></div><br><br><br>`;
+
+          buttonOption = shuffleArray(buttonOption);
+
+          return cardStart +
+              legend +
+              buttonQuestion +
+              translate +
+              buttonOption[0] +
+              buttonOption[1] +
+              buttonOption[2] +
+              buttonOption[3] +
+              cardFinish;
+      }
+  }
 }
