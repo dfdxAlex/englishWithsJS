2439d90c7547c681a88dd2338663f6b84d0f9626
oleksandr-ishchuk <alexmway@mail.ru>
2024-12-22 00:38:26 +0100

    bag fix for DB English

--- js/models/filedb/2/irregularVerbsV3.js
+++ js/models/filedb/2/irregularVerbsV3.js
@@ -1,45 +1,24 @@
 const irregularVerbsV3 = [
-    // Example for "be"
     ["be", "been", "was", "wer", "ben", "быть", "бути", "być"],
-    // Example for "break"
     ["break", "broken", "broke", "braek", "brocken", "ломать", "ламати", "łamać"],
-    // Example for "buy"
     ["buy", "bought", "buoght", "bey", "baut", "покупать", "купувати", "kupować"],
-    // Example for "choose"
     ["choose", "chosen", "chose", "choosen", "chossen", "выбирать", "вибирати", "wybierać"],
-    // Example for "do"
     ["do", "done", "did", "doo", "dne", "делать", "робити", "robić"],
-    // Example for "eat"
     ["eat", "eaten", "ate", "et", "eatn", "есть", "їсти", "jeść"],
-    // Example for "go"
     ["go", "gone", "went", "gon", "goen", "идти", "йти", "iść"],
-    // Example for "have"
     ["have", "had", "haved", "haev", "hadn", "иметь", "мати", "mieć"],
-    // Example for "make"
     ["make", "made", "maked", "maek", "mdae", "делать", "робити", "robić"],
-    // Example for "see"
     ["see", "seen", "saw", "se", "sen", "видеть", "бачити", "widzieć"],
-    // Example for "take"
     ["take", "taken", "took", "taek", "takn", "брать", "брати", "brać"],
-    // Example for "write"
     ["write", "written", "wrote", "writ", "wrtten", "писать", "писати", "pisać"],
-    // Example for "speak"
     ["speak", "spoken", "spoke", "spaek", "spokn", "говорить", "говорити", "mówić"],
-    // Example for "read"
     ["read", "read", "red", "reed", "rad", "читать", "читати", "czytać"],
-    // Example for "drive"
     ["drive", "driven", "drove", "driv", "droven", "водить", "водити", "prowadzić"],
-    // Example for "begin"
     ["begin", "begun", "began", "bginn", "begun", "начинать", "починати", "zaczynać"],
-    // Example for "find"
     ["find", "found", "founded", "fynd", "foun", "находить", "знаходити", "znaleźć"],
-    // Example for "think"
     ["think", "thought", "thoght", "thynk", "thouht", "думать", "думати", "myśleć"],
-    // Example for "give"
     ["give", "given", "gave", "giv", "gven", "давать", "давати", "dawać"],
-    // Example for "know"
     ["know", "known", "knew", "knw", "knwon", "знать", "знати", "wiedzieć"],
-    // Added entries to extend to 100
     ["become", "become", "became", "becm", "becomm", "становиться", "ставати", "stawać się"],
     ["begin", "begun", "began", "bgin", "begnn", "начинать", "починати", "zaczynać"],
     ["blow", "blown", "blew", "blo", "bloon", "дуть", "дути", "wiać"],
