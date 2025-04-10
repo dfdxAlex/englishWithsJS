dde2021191886f50d31c91762f858cb74854f13e
oleksandr-ishchuk <alexmway@mail.ru>
2025-04-10 17:03:34 +0200

    add test questions for passive 2

--- js/models/filedb/1/prContiniusPerfectPassive.js
+++ js/models/filedb/1/prContiniusPerfectPassive.js
@@ -51,7 +51,402 @@ const prContiniusPerfectPassive = [
     ["Has the government passed the law?", "The law has been passed.", "The law was passed in 2021.", "The law is passed when necessary.", "The law is being passed now.", "Правительство приняло закон?", "Уряд прийняв закон?", "Czy ustawa została uchwalona?"],  
     ["Has the scientist discovered a new element?", "A new element has been discovered.", "A new element was discovered last decade.", "New elements are discovered rarely.", "A new element is being discovered right now.", "Ученый открыл новый элемент?", "Вчений відкрив новий елемент?", "Czy nowy pierwiastek został odkryty?"],  
     ["Has the airline canceled the flight?", "The flight has been canceled.", "The flight was canceled this morning.", "Flights are canceled due to weather conditions.", "The flight is being canceled now.", "Авиакомпания отменила рейс?", "Авіакомпанія скасувала рейс?", "Czy lot został odwołany?"],
+    ["Dinner is being prepared in the kitchen.",
 
+    "What is being prepared in the kitchen?",
+  
+    "What has been prepared in the kitchen?",       // другое время
+    "What was being prepared in the kitchen?",      // другое время
+    "Who is preparing dinner in the kitchen?",      // активный залог
+  
+    "Ужин готовится на кухне.",
+    "Вечерю готують на кухні.",
+    "Obiad jest przygotowywany w kuchni."],
+    [  "The room has been cleaned thoroughly.",
+
+        "What has been cleaned thoroughly?",
+      
+        "What is being cleaned thoroughly?",           // настоящее длительное в пассиве
+        "What had been cleaned thoroughly before?",    // past perfect passive
+        "Who cleaned the room thoroughly?",            // активный залог
+      
+        "Комната была тщательно убрана.",
+        "Кімнату було ретельно прибрано.",
+        "Pokój został dokładnie posprzątany."],
+    [  "The floor is being mopped right now.",
+
+        "What is being mopped right now?",
+      
+        "What has been mopped?",                       // другое время
+        "What was being mopped yesterday?",            // past continuous passive
+        "Who is mopping the floor right now?",         // активный залог
+      
+        "Пол сейчас моют.",
+        "Підлогу зараз миють.",
+        "Podłoga jest teraz myta."],
+    [  "The groceries have been delivered.",
+
+        "What has been delivered?",
+      
+        "What is being delivered?",                    // настоящее длительное
+        "What had been delivered before noon?",        // past perfect passive
+        "Who delivered the groceries?",                // актив
+      
+        "Продукты были доставлены.",
+        "Продукти були доставлені.",
+        "Zakupy zostały dostarczone."],
+    [  "The carpet is being vacuumed at the moment.",
+
+        "What is being vacuumed at the moment?",
+      
+        "What has been vacuumed?",                     // present perfect passive
+        "What was being vacuumed when you arrived?",   // past continuous passive
+        "Who is vacuuming the carpet at the moment?",  // актив
+      
+        "Ковёр сейчас пылесосят.",
+        "Килим зараз пилососять.",
+        "Dywan jest teraz odkurzany."],
+    [  "The fridge has been repaired recently.",
+
+        "What has been repaired recently?",
+      
+        "What is being repaired right now?",               // настоящее длительное
+        "What had been repaired before you came?",         // past perfect
+        "Who repaired the fridge recently?",               // активный залог
+      
+        "Холодильник недавно отремонтировали.",
+        "Холодильник нещодавно відремонтували.",
+        "Lodówka została niedawno naprawiona."],
+    [  "The bathroom is being renovated now.",
+
+        "What is being renovated now?",
+      
+        "What has been renovated recently?",               // present perfect
+        "What was being renovated last week?",             // past continuous
+        "Who is renovating the bathroom now?",             // актив
+      
+        "Ванная сейчас ремонтируется.",
+        "Ванну зараз ремонтують.",
+        "Łazienka jest teraz remontowana."],
+    [  "The dishes have been washed already.",
+
+        "What has been washed already?",
+      
+        "What is being washed now?",                       // настоящее длительное
+        "What had been washed before dinner?",             // past perfect
+        "Who washed the dishes already?",                  // актив
+      
+        "Посуда уже вымыта.",
+        "Посуд уже помили.",
+        "Naczynia zostały już umyte."],
+    [  "The garden is being watered right now.",
+
+        "What is being watered right now?",
+      
+        "What has been watered today?",                    // present perfect
+        "What was being watered in the morning?",          // past continuous
+        "Who is watering the garden right now?",           // актив
+      
+        "Сад сейчас поливают.",
+        "Сад зараз поливають.",
+        "Ogród jest teraz podlewany."],
+    [  "The package has been sent.",
+
+        "What has been sent?",
+      
+        "What is being sent right now?",                   // настоящее длительное
+        "What had been sent before the weekend?",          // past perfect
+        "Who sent the package?",                           // актив
+      
+        "Посылка была отправлена.",
+        "Посилку було відправлено.",
+        "Paczka została wysłana."],
+    [  "The windows are being cleaned now.",
+
+        "What is being cleaned now?",
+      
+        "What has been cleaned already?",               // present perfect
+        "What was being cleaned yesterday?",            // past continuous
+        "Who is cleaning the windows right now?",       // актив
+      
+        "Окна сейчас моют.",
+        "Вікна зараз миють.",
+        "Okna są teraz myte."],
+    [  "The report has been finished by the team.",
+
+        "What has been finished by the team?",
+      
+        "What is being finished by the team?",            // настоящее длительное
+        "What had been finished before the meeting?",    // past perfect
+        "Who finished the report?",                      // активный залог
+      
+        "Отчёт был завершён командой.",
+        "Звіт був завершений командою.",
+        "Raport został zakończony przez zespół."],
+    [  "The car is being washed at the moment.",
+
+        "What is being washed at the moment?",
+      
+        "What has been washed recently?",                 // present perfect
+        "What was being washed when you passed by?",      // past continuous
+        "Who is washing the car at the moment?",          // актив
+      
+        "Машину сейчас моют.",
+        "Машину зараз миють.",
+        "Samochód jest teraz myty."],
+    [  "The keys have been found.",
+
+        "What has been found?",
+      
+        "What is being found?",                           // настоящее длительное
+        "What had been found earlier?",                   // past perfect
+        "Who found the keys?",                            // активный залог
+      
+        "Ключи были найдены.",
+        "Ключі були знайдені.",
+        "Klucze zostały znalezione."],
+    [  "The documents are being reviewed by the manager.",
+
+        "What is being reviewed by the manager?",
+      
+        "What has been reviewed by the manager?",         // present perfect
+        "What was being reviewed when you called?",       // past continuous
+        "Who is reviewing the documents?",                // активный залог
+      
+        "Документы сейчас просматриваются менеджером.",
+        "Документи зараз переглядаються менеджером.",
+        "Dokumenty są teraz przeglądane przez menedżera."],
+    [  "The email has been sent successfully.",
+
+        "What has been sent successfully?",
+      
+        "What is being sent right now?",                   // настоящее длительное
+        "What had been sent before you replied?",          // past perfect
+        "Who sent the email successfully?",                // активный залог
+      
+        "Письмо было отправлено успешно.",
+        "Емейл було надіслано успішно.",
+        "E-mail został wysłany pomyślnie."],
+    [  "The video is being edited at the moment.",
+
+        "What is being edited at the moment?",
+      
+        "What has been edited already?",                   // present perfect
+        "What was being edited when you checked?",         // past continuous
+        "Who is editing the video at the moment?",         // активный залог
+      
+        "Видео сейчас редактируется.",
+        "Відео зараз редагується.",
+        "Wideo jest teraz edytowane."],
+    [  "The documents have been printed.",
+
+        "What has been printed?",
+      
+        "What is being printed right now?",                // настоящее длительное
+        "What had been printed earlier?",                  // past perfect
+        "Who printed the documents?",                      // активный залог
+      
+        "Документы были распечатаны.",
+        "Документи були надруковані.",
+        "Dokumenty zostały wydrukowane."],
+    [  "The cake is being baked in the oven.",
+
+        "What is being baked in the oven?",
+      
+        "What has been baked in the oven?",                 // present perfect
+        "What was being baked when you came in?",          // past continuous
+        "Who is baking the cake in the oven?",             // активный залог
+      
+        "Торт сейчас печётся в духовке.",
+        "Торт зараз печеться в духовці.",
+        "Ciasto jest teraz pieczone w piekarniku."],
+    [  "The room has been decorated beautifully.",
+
+        "What has been decorated beautifully?",
+      
+        "What is being decorated beautifully?",            // настоящее длительное
+        "What had been decorated beautifully before the event?", // past perfect
+        "Who decorated the room beautifully?",             // активный залог
+      
+        "Комната была красиво украшена.",
+        "Кімната була красиво прикрашена.",
+        "Pokój został pięknie udekorowany."],
+    [  "The lecture is being given by the professor right now.",
+
+        "What is being given by the professor right now?",
+      
+        "What has been given during the course?",            // present perfect
+        "What was being given during the last class?",      // past continuous
+        "Who is giving the lecture right now?",             // активный залог
+      
+        "Лекция сейчас читается профессором.",
+        "Лекцію зараз читає професор.",
+        "Wykład jest teraz prowadzony przez profesora."],
+    [  "The homework has been completed by the student.",
+
+        "What has been completed by the student?",
+      
+        "What is being completed by the student?",            // настоящее длительное
+        "What had been completed by the student before the deadline?", // past perfect
+        "Who completed the homework?",                         // активный залог
+      
+        "Домашнее задание было выполнено студентом.",
+        "Домашнє завдання було виконано студентом.",
+        "Praca domowa została ukończona przez studenta."],
+    [  "The book is being read by the student at the moment.",
+
+        "What is being read by the student at the moment?",
+      
+        "What has been read by the student recently?",       // present perfect
+        "What was being read when you arrived?",              // past continuous
+        "Who is reading the book at the moment?",             // активный залог
+      
+        "Книгу сейчас читает студент.",
+        "Книгу зараз читає студент.",
+        "Książka jest teraz czytana przez studenta."],
+    [  "The puzzle has been completed.",
+
+        "What has been completed?",
+      
+        "What is being completed?",                            // настоящее длительное
+        "What had been completed before you came?",            // past perfect
+        "Who completed the puzzle?",                           // активный залог
+      
+        "Пазл был собран.",
+        "Пазл був зібраний.",
+        "Układanka została ułożona."],
+    [  "The hiking trail is being marked at the moment.",
+
+        "What is being marked at the moment?",
+      
+        "What has been marked on the trail?",                  // present perfect
+        "What was being marked when you passed by?",           // past continuous
+        "Who is marking the hiking trail?",                    // активный залог
+      
+        "Тропа для походов сейчас маркируется.",
+        "Стежка для походів зараз маркується.",
+        "Szlak turystyczny jest teraz oznaczany."],
+    [  "The vacation has been planned for next month.",
+
+        "What has been planned for next month?",
+      
+        "What is being planned for the weekend?",              // настоящее длительное
+        "What had been planned before the trip?",              // past perfect
+        "Who planned the vacation for next month?",            // активный залог
+      
+        "Отпуск был запланирован на следующий месяц.",
+        "Відпустка була запланована на наступний місяць.",
+        "Urlop został zaplanowany na przyszły miesiąc."],
+    [  "The concert is being organized by the local committee.",
+
+        "What is being organized by the local committee?",
+      
+        "What has been organized for the concert?",            // present perfect
+        "What was being organized when you called?",           // past continuous
+        "Who is organizing the concert?",                      // активный залог
+      
+        "Концерт сейчас организуется местным комитетом.",
+        "Концерт зараз організовується місцевим комітетом.",
+        "Koncert jest teraz organizowany przez lokalny komitet."],
+    [  "The art class has been attended by many students.",
+
+        "What has been attended by many students?",
+      
+        "What is being attended by many students?",            // настоящее длительное
+        "What had been attended by many students before the event?", // past perfect
+        "Who attended the art class?",                         // активный залог
+      
+        "На уроке рисования было много студентов.",
+        "На урок малювання було багато студентів.",
+        "Na lekcji sztuki było wielu uczniów."],
+    [  "The fishing trip is being planned right now.",
+
+        "What is being planned right now?",
+      
+        "What has been planned for the trip?",                  // present perfect
+        "What was being planned when you arrived?",             // past continuous
+        "Who is planning the fishing trip?",                    // активный залог
+      
+        "Рыболовная поездка сейчас планируется.",
+        "Риболовна поїздка зараз планується.",
+        "Wyprawa wędkarska jest teraz planowana."],
+    [  "The painting has been created by the artist.",
+
+        "What has been created by the artist?",
+      
+        "What is being created by the artist?",               // настоящее длительное
+        "What had been created by the artist before the exhibition?", // past perfect
+        "Who created the painting?",                          // активный залог
+      
+        "Картина была создана художником.",
+        "Картина була створена художником.",
+        "Obraz został stworzony przez artystę."],
+    [  "The new project is being discussed in the meeting.",
+
+        "What is being discussed in the meeting?",
+      
+        "What has been discussed in the meeting so far?",      // present perfect
+        "What was being discussed when you left?",             // past continuous
+        "Who is discussing the new project in the meeting?",   // активный залог
+      
+        "Новый проект сейчас обсуждается на собрании.",
+        "Новий проект зараз обговорюється на нараді.",
+        "Nowy projekt jest omawiany na spotkaniu."],
+    [  "The game has been played by the children.",
+
+        "What has been played by the children?",
+      
+        "What is being played right now?",                    // настоящее длительное
+        "What had been played before dinner?",                // past perfect
+        "Who played the game?",                               // активный залог
+      
+        "Игра была сыграна детьми.",
+        "Гра була зіграна дітьми.",
+        "Gra została rozegrana przez dzieci."],
+    [  "The house is being painted this week.",
+
+        "What is being painted this week?",
+      
+        "What has been painted already?",                     // present perfect
+        "What was being painted when you visited?",           // past continuous
+        "Who is painting the house this week?",               // активный залог
+      
+        "Дом сейчас красят на этой неделе.",
+        "Дім зараз фарбують цього тижня.",
+        "Dom jest malowany w tym tygodniu."],
+    [  "The workshop has been organized for next weekend.",
+
+        "What has been organized for next weekend?",
+      
+        "What is being organized for the weekend?",           // настоящее длительное
+        "What had been organized before you joined?",        // past perfect
+        "Who organized the workshop?",                        // активный залог
+      
+        "Мастер-класс был организован на следующие выходные.",
+        "Майстер-клас був організований на наступні вихідні.",
+        "Warsztat został zorganizowany na przyszły weekend."],
+    [  "The camera is being tested now.",
+
+        "What is being tested now?",
+      
+        "What has been tested already?",                      // present perfect
+        "What was being tested when you arrived?",            // past continuous
+        "Who is testing the camera now?",                     // активный залог
+      
+        "Камера сейчас тестируется.",
+        "Камера зараз тестується.",
+        "Kamera jest teraz testowana."],
+    [  "The new software has been installed on the computer.",
+
+        "What has been installed on the computer?",
+      
+        "What is being installed on the computer?",            // настоящее длительное
+        "What had been installed before the update?",         // past perfect
+        "Who installed the new software?",                    // активный залог
+      
+        "Новое программное обеспечение было установлено на компьютер.",
+        "Нове програмне забезпечення було встановлено на комп'ютер.",
+        "Nowe oprogramowanie zostało zainstalowane na komputerze."],
 ];
 
     // Если это свойство есть, то оно попадает в заголовок теста
