247614a2889e073857ac1842c7bd0af28e510a98
oleksandr-ishchuk <alexmway@mail.ru>
2025-04-17 22:14:14 +0200

    code cleaned

--- js/controllers/activatingLogs.js
+++ js/controllers/activatingLogs.js
@@ -51,22 +51,4 @@ function activatingLogs(regim = false, ev = null)
           }
         }
       );
-
-
-    // Дальше алгоритм прятания рабочего поля либо логов
-  //   const searchErrorButton = document.getElementById('search_error');
-  //   const searchErrorLog = document.getElementById('log');
-  //   const searchErrorWorkField = document.getElementById('working_field');
-  //   const logForDiv = document.getElementById('log_for_div');
-  // if (regim) {
-  //   searchErrorButton.style.display = 'none';
-  //   searchErrorLog.style.display = 'none';
-  //   searchErrorWorkField.style.display = 'none';
-  //   logForDiv.style.display = 'block';
-  // } else {
-  //   searchErrorButton.style.display = 'block';
-  //   searchErrorLog.style.display = 'block';
-  //   searchErrorWorkField.style.display = 'block';
-  //   logForDiv.style.display = 'none';
-  // }
 }
--- js/controllers/diament.js
+++ js/controllers/diament.js
@@ -5,5 +5,4 @@ function diament()
     let diament = 0;
     [emodzi, smile, diament] = diamentReturnStr();
     diamentView(diament, emodzi, smile);
-    //daimentInfo(); // Функция стала самозапускающейся
 }
