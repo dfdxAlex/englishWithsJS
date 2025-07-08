c81b324c6f13a8660a3f102a3e1ebc5abd599f66
Dfdx Alex <alexmway78@gmail.com>
2025-07-09 00:36:28 +0200

    add style for gneral item

--- js/views/MenuRight.js
+++ js/views/MenuRight.js
@@ -20,19 +20,51 @@ class MenuRight
         let rez = '<div class="btn-group" role="group" aria-label="Button group with nested dropdown">';
         rez+='<div class="btn-group" role="group">';
         
+        // Here the main menu buttons are formed, Select test, Edit words...
         rez+='<button class="btn btn-light" id="btnGroupDrop1" type="button" data-bs-toggle="dropdown" aria-expanded="false">';
         rez+=this.nameMenu;
         rez+='</button>';
 
         rez+='<ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">';
         
+        // menu-specific parity counters
+        let countItemOne=0;
+        let countItemTwo=0;
+
         rez+= this.nameButtons.map((element, index) => {
-            // статический метод mapNameMenu(index) содержит инфу
-            // о том, конкретный перебираемый пункт в каким меню 
-            // должен находиться
+            // The method returns the location of a specific item. LevelDataModel.mapNameMenu()
             let numberMenuLocal = LevelDataModel.mapNameMenu(index);
-            if (numberMenuLocal !== this.numberMenu) return;
-            return `<li><a class="dropdown-item" id="level${index+1}">${element}</a></li>`;
+            if (numberMenuLocal !== this.numberMenu) {
+                return;
+            }
+
+
+            // Add one of the classes to a specific test item.
+            let addClass = "";
+            let id = index+1;
+
+            if (numberMenuLocal === 1) {
+                countItemOne++;
+                if (countItemOne % 2 == 1) 
+                {
+                    addClass = "style-for-item-one";
+                } else {
+                    addClass = "style-for-item-two";
+                }
+            }
+            if (numberMenuLocal === 2) {
+                countItemTwo++;
+                if (countItemTwo % 2 == 1) 
+                {
+                    addClass = "style-for-item-one";
+                } else {
+                    addClass = "style-for-item-two";
+                }
+            }     
+
+            addClass = " style-for-item-start " + addClass;
+
+            return `<li><a class="dropdown-item ${addClass}" id="level${id}">${element}</a></li>`;
         }).join('');
 
         rez+='</ul>';
