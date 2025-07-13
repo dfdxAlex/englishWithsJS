da34e61ab26577be37f5b473dd497711b26f10b1
Dfdx Alex <alexmway78@gmail.com>
2025-07-14 01:20:40 +0200

    add return kategiria Test

--- js/controllers/MenuController.js
+++ js/controllers/MenuController.js
@@ -9,32 +9,46 @@ class MenuController
 
     init() 
     {
+        // levelDataModel.getArrayNameButton() - возвращает массив с названиями кнопок
+        // menu.getSeedForMenu() - выбирает нужный контейнер для кнопок
+
         // создать объект правого меню
         const menu = new MenuRight(this.levelDataModel.getArrayNameButton());
+
         // создать объект второго правого меню
         const menu2 = new MenuLearnWord(this.levelDataModel.getArrayNameButton());
+
         // создать объект второго правого меню
         const menu3 = new MenuHelp(this.levelDataModel.getArrayNameButton());
+
         // создать объект третьего правого меню
         const menu4 = new MenuStatictic(this.levelDataModel.getArrayNameButton());
+
         // найти точку для публикации правого меню
         // само место парковки задаётся внутри класса MenuRight
         const point = menu.getSeedForMenu();
+
         // найти точку для публикации правого меню
         // само место парковки задаётся внутри класса MenuLearnWord
         const point2 = menu2.getSeedForMenu();
+
         // найти точку для публикации правого меню
         // само место парковки задаётся внутри класса MenuLearnWord
         const point3 = menu3.getSeedForMenu();
+
         // найти точку для публикации правого меню
         // само место парковки задаётся внутри класса MenuLearnWord
         const point4 = menu4.getSeedForMenu();
         
         // поместить меню на выбранное место.
         point.innerHTML = menu.menuDropdown2();
+        dellPrimaryItemForUpMenu(); // добавляет клик для самоудаления
+        returnPrimaryItemForUpMenu(); // добавить клик на все пункты, чтобы вернуть главную кнопку
         point2.innerHTML = menu2.menuDropdown2();
         point3.innerHTML = menu3.menuDropdown2();
         point4.innerHTML = menu4.menuDropdown2();
+
+        // document.getElementById('btnGroupDrop1').onclick = () => point.style.display = 'none'; 
 
         const unit = [];
         for (let i=1; i<=this.levelDataModel.getTic(); i++) {
@@ -46,6 +60,14 @@ class MenuController
             } 
             unit[i] = document.getElementById('level'+i);
             unit[i].onclick = handleLevelX.bind(null, new LevelDataModel(i));
+            // unit[i].addEventListener('onclick', handleLevelX.bind(null, new LevelDataModel(i)));
         }
     }
+
+
 }
+
+
+
+
+
--- js/views/MenuRight.js
+++ js/views/MenuRight.js
@@ -1,4 +1,6 @@
 // класс показывает правое меню
+// данный класс насследуется другими классами, поэтому проверяются категории 1 и 2, хотя
+// в самом конструкторе фигурирует категория 1 this.numberMenu = 1;
 
 class MenuRight
 {
@@ -13,64 +15,93 @@ class MenuRight
         // используется как внутрри класса, так и за его пределами.
         this.seedMenu = 'burger';
         this.numberMenu = 1;
+
+        // menu-specific parity counters
+        // Переменные используются для счётчика чётности пунктов меню
+        this.countItemOne=0;
+        this.countItemTwo=0;
+        // Свойство хранит номер категории, в которой должен быть конкретный пункт
+        this.numberMenuLocal=0;
     }
     
     menuDropdown2()
     {
-        let rez = '<div class="btn-group" role="group" aria-label="Button group with nested dropdown">';
+        let rez = `<div 
+                    id="container-menu-up"
+                    class="btn-group" 
+                    role="group" 
+                    aria-label="Button group with nested dropdown"
+                   >`;
         rez+='<div class="btn-group" role="group">';
         
         // Here the main menu buttons are formed, Select test, Edit words...
-        rez+='<button class="btn btn-light" id="btnGroupDrop1" type="button" data-bs-toggle="dropdown" aria-expanded="false">';
+        rez+=`<button 
+                class="btn btn-light" 
+                id="btnGroupDrop1" 
+                type="button" 
+                data-bs-toggle="dropdown" 
+                aria-expanded="false"
+              >`;
+
         rez+=this.nameMenu;
         rez+='</button>';
 
-        rez+='<ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">';
+        rez+='<ul class="dropdown-menu" aria-labelledby="btnGroupDrop1" id = "itemSelectTest">';
         
-        // menu-specific parity counters
-        let countItemOne=0;
-        let countItemTwo=0;
 
-        rez+= this.nameButtons.map((element, index) => {
+        // перебрать массив с названиями пунктов для меню
+        rez += this.nameButtons.map((element, index) => {
             // The method returns the location of a specific item. LevelDataModel.mapNameMenu()
-            let numberMenuLocal = LevelDataModel.mapNameMenu(index);
-            if (numberMenuLocal !== this.numberMenu) {
+            this.numberMenuLocal = LevelDataModel.mapNameMenu(index);
+            if (this.numberMenuLocal !== this.numberMenu) {
                 return;
             }
 
-
-            // Add one of the classes to a specific test item.
-            let addClass = "";
-            let id = index+1;
-
-            if (numberMenuLocal === 1) {
-                countItemOne++;
-                if (countItemOne % 2 == 1) 
-                {
-                    addClass = "style-for-item-one";
-                } else {
-                    addClass = "style-for-item-two";
-                }
-            }
-            if (numberMenuLocal === 2) {
-                countItemTwo++;
-                if (countItemTwo % 2 == 1) 
-                {
-                    addClass = "style-for-item-one";
-                } else {
-                    addClass = "style-for-item-two";
-                }
-            }     
-
-            addClass = " style-for-item-start " + addClass;
-
-            return `<li><a class="dropdown-item ${addClass}" id="level${id}">${element}</a></li>`;
-        }).join('');
+            return `<li><a class="dropdown-item ${this.addClass()}" id="level${index+1}">${element}</a></li>`;
+        }).join(''); 
 
         rez+='</ul>';
         rez+='</div></div>';
-
+        // returnPrimaryItemForUpMenu();
         return rez;
+    }
+
+    // Функция проанализирует какой категории принадлежит элемент меню. Это тесты или Учить слова
+    // После этого функция вернет один из двух возможных классов, в зависимости от чётной или 
+    // нечетной позиции конкретной кнопки
+    addClass()
+    {
+        // Здесь будет храниться имя класса для конкретной кнопки
+        let addClass = "";
+
+        // Если рисуем меню с выбором тестов, назначение пункта меню = 1
+        // Это необходимо потому, что меню главное формируется подряд, но не по очереди в каждом
+        // подпункте. Поэтому просто менять класс через один не получается, нужно определить
+        // сначала в какой категории будет пункт и потом его разукрашивать как чётный или нет 
+        if (this.numberMenuLocal === 1) {
+            this.countItemOne++;
+            if (this.countItemOne % 2 == 1) 
+            {
+                addClass = "style-for-item-one";
+            } else {
+                addClass = "style-for-item-two";
+            }
+        }
+        // Если рисуем меню с выбором учить слова, назначение пункта меню = 2
+        // Это необходимо потому, что меню главное формируется подряд, но не по очереди в каждом
+        // подпункте. Поэтому просто менять класс через один не получается, нужно определить
+        // сначала в какой категории будет пункт и потом его разукрашивать как чётный или нет 
+        if (this.numberMenuLocal === 2) {
+            this.countItemTwo++;
+            if (this.countItemTwo % 2 == 1) 
+            {
+                addClass = "style-for-item-one";
+            } else {
+                addClass = "style-for-item-two";
+            }
+        }   
+        
+        return " style-for-item-start " + addClass;
     }
 
     getSeedForMenu()
@@ -79,3 +110,20 @@ class MenuRight
     }
 }
 
+   // Функция должна спрятать лишнее при открытии меню выбора теста.
+   function dellPrimaryItemForUpMenu()
+    {
+        let generalItem = document.getElementById('btnGroupDrop1');
+        generalItem.addEventListener('click', ()=>{generalItem.style.display = 'none'; });
+    }
+
+   // Накидывает событие возврата категории меню при выборе любого теста. Событие на 
+   // контейнере, который содержит в себе все тесты и срабатывает эффект всплытия.
+   function returnPrimaryItemForUpMenu()
+    {
+        let generalItem = document.getElementById('itemSelectTest');
+        generalItem.addEventListener('click', ()=>{
+            let generalItemP = document.getElementById('btnGroupDrop1');
+            generalItemP.style.display = 'block'; 
+        });
+    }
