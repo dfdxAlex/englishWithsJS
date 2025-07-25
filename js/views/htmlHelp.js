69cabd145b6aa3cec7dc8748f2531fcccc7ec59e
Dfdx Alex <alexmway78@gmail.com>
2025-07-25 02:42:38 +0200

    Updates

--- js/views/htmlHelp.js
+++
@@ -1,9 +0,0 @@
-function htmlHelp()
-{
-    document.getElementById('html-help').innerHTML = `
-        <div class="row w-100 mb-3">
-            <div id="exercise" ></div>
-            <div id="help-test-legend"></div>
-        </div>
-    `;
-}
--- vite.config.js
+++
@@ -1,4 +0,0 @@
-export default defineConfig({
-  base: '/englishWithsJS/', // путь к репозиторию
-  // ... другие настройки
-})
