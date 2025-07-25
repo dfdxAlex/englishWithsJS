b6b259157a811825de2aaddeadd7b90b6024551c
Dfdx Alex <alexmway78@gmail.com>
2025-07-25 17:07:20 +0200

    Updates

--- englishWithsJS/libraryJs/navbar/navbarStart.js
+++
@@ -1,7 +0,0 @@
-
-// Указать ИД для размещения меню.
-PropertysNavbar.idMenu = 'burger';
-PropertysNavbar.logo_images = 'src/logo.png';
-PropertysNavbar.setMenu();
-
-console.log(PropertysNavbar.menu);
