b35cf1703aa0e2d3420667a4851793cc79b96612
Dfdx Alex <alexmway78@gmail.com>
2025-07-31 19:26:49 +0200

    going to a modules

--- src/controllers/Thoury.js
+++ src/controllers/Thoury.js
@@ -1,30 +1,7 @@
 import './styles/thoury.scss';
 import { FactoryRegistr } from '../models/FactoryRegistr.js';
 import { getHeaderWindow } from '../models/forThoury/getHeaderWindow.js';
-import { returnInfoUnit22RU, 
-         returnLevel42,
-         returnLevel40,
-         returnLevel39,
-         returnLevel38,
-         returnLevel37,
-         returnLevel20,
-         returnLevel36,
-         returnLevel35,
-         returnLevel34, 
-         returnLevel33, 
-         returnLevel32,
-         returnLevel31,
-         returnLevel23,
-         returnLevel21,
-         returnLevel19,
-         returnLevel18,
-         returnLevel17,
-         returnLevel16,
-         returnLevel15,
-         returnLevel14,
-         returnLevel10,
-         returnLevel11,
-         returnLevel13 } from './forThoury/indexForThoury.js';
+import { getBodyThoury } from '../models/forThoury/getBodyThoury.js';
 
 export class Thoury
 {
@@ -55,59 +32,7 @@ static handleThoury(level)
     // Функция помещает информацию в тело раздела Теория
     static bodyThoury()
     {
-      switch (this.level) {
-      case 43: 
-        return this.translate.translate(returnInfoUnit22RU());
-      case 42: 
-        return this.translate.translate(returnLevel42());
-      case 40:
-        return this.translate.translate(returnLevel40());
-      case 39: 
-        return this.translate.translate(returnLevel39());
-      case 38:
-        return this.translate.translate(returnLevel38());
-      case 37:
-        return this.translate.translate(returnLevel37());
-      case 20: 
-        return this.translate.translate(returnLevel20());
-      case 36:
-        return this.translate.translate(returnLevel36());
-      case 35:
-        return this.translate.translate(returnLevel35());
-      case 34:
-        return this.translate.translate(returnLevel34());
-      case 33:
-        return this.translate.translate(returnLevel33());
-      case 32:
-        return this.translate.translate(returnLevel32());
-      case 31:
-        return this.translate.translate(returnLevel31());
-      case 23:
-        return this.translate.translate(returnLevel23());
-      case 21:
-        return this.translate.translate(returnLevel21());
-      case 19: 
-        return this.translate.translate(returnLevel19());
-      case 18:
-        return this.translate.translate(returnLevel18());
-      case 17:
-        return this.translate.translate(returnLevel17());
-      case 16:
-        return this.translate.translate(returnLevel16());
-      case 15: 
-        return this.translate.translate(returnLevel15());
-      case 14:
-        return this.translate.translate(returnLevel14());
-      case 9:
-      case 10: 
-        return this.translate.translate(returnLevel10());
-      case 11:
-        return this.translate.translate(returnLevel11());
-      case 13:
-        return this.translate.translate(returnLevel13());
-      default:
-        return this.translate.translate('Просто учим слова');
-     }
+        return getBodyThoury(this);
     }
 }
 
---
+++ src/models/forThoury/getBodyThoury.js
@@ -0,0 +1,81 @@
+import { returnInfoUnit22RU, 
+         returnLevel42,
+         returnLevel40,
+         returnLevel39,
+         returnLevel38,
+         returnLevel37,
+         returnLevel20,
+         returnLevel36,
+         returnLevel35,
+         returnLevel34, 
+         returnLevel33, 
+         returnLevel32,
+         returnLevel31,
+         returnLevel23,
+         returnLevel21,
+         returnLevel19,
+         returnLevel18,
+         returnLevel17,
+         returnLevel16,
+         returnLevel15,
+         returnLevel14,
+         returnLevel10,
+         returnLevel11,
+         returnLevel13 } from '../../controllers/forThoury/indexForThoury.js';
+
+export function getBodyThoury(thisS)
+{
+    switch (thisS.level) {
+      case 43: 
+        return thisS.translate.translate(returnInfoUnit22RU());
+      case 42: 
+        return thisS.translate.translate(returnLevel42());
+      case 40:
+        return thisS.translate.translate(returnLevel40());
+      case 39: 
+        return thisS.translate.translate(returnLevel39());
+      case 38:
+        return thisS.translate.translate(returnLevel38());
+      case 37:
+        return thisS.translate.translate(returnLevel37());
+      case 20: 
+        return thisS.translate.translate(returnLevel20());
+      case 36:
+        return thisS.translate.translate(returnLevel36());
+      case 35:
+        return thisS.translate.translate(returnLevel35());
+      case 34:
+        return thisS.translate.translate(returnLevel34());
+      case 33:
+        return thisS.translate.translate(returnLevel33());
+      case 32:
+        return thisS.translate.translate(returnLevel32());
+      case 31:
+        return thisS.translate.translate(returnLevel31());
+      case 23:
+        return thisS.translate.translate(returnLevel23());
+      case 21:
+        return thisS.translate.translate(returnLevel21());
+      case 19: 
+        return thisS.translate.translate(returnLevel19());
+      case 18:
+        return thisS.translate.translate(returnLevel18());
+      case 17:
+        return thisS.translate.translate(returnLevel17());
+      case 16:
+        return thisS.translate.translate(returnLevel16());
+      case 15: 
+        return thisS.translate.translate(returnLevel15());
+      case 14:
+        return thisS.translate.translate(returnLevel14());
+      case 9:
+      case 10: 
+        return thisS.translate.translate(returnLevel10());
+      case 11:
+        return thisS.translate.translate(returnLevel11());
+      case 13:
+        return thisS.translate.translate(returnLevel13());
+      default:
+        return thisS.translate.translate('Просто учим слова');
+     }
+}
