1ec2f95e54ab02f97c38965b63c79e1c676bd70c
Dfdx Alex <alexmway78@gmail.com>
2025-09-08 00:35:32 +0200

    add new backround

--- src/view/styles/bodyImages/setBackground.js
+++ src/view/styles/bodyImages/setBackground.js
@@ -1,4 +1,27 @@
-
+import background1 from './background1.jpg';
+import background2 from './background2.jpg';
+import background3 from './background3.jpg';
+import background4 from './background4.jpg';
+import background5 from './background5.jpg';
+import background6 from './background6.jpg';
+import background7 from './background7.jpg';
+import background8 from './background8.jpg';
+import background9 from './background9.jpg';
+import background10 from './background10.jpg';
+import background11 from './background11.jpg';
+import background12 from './background12.jpg';
+import background13 from './background13.jpg';
+import background14 from './background14.jpg';
+import background15 from './background15.jpg';
+import background16 from './background16.jpg';
+import background17 from './background17.jpg';
+import background18 from './background18.jpg';
+import background19 from './background19.jpg';
+import background20 from './background20.jpg';
+import background21 from './background21.jpg';
+import background22 from './background22.jpg';
+import background23 from './background23.jpg';
+import background24 from './background24.jpg';
 
 export function setBackground()
 {
