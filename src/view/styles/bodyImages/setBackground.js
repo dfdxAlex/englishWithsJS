bfc34f49abb14272d5a7e211a2b1ea9834bf80d1
Dfdx Alex <alexmway78@gmail.com>
2025-09-08 14:42:59 +0200

    add new backround

--- src/view/styles/bodyImages/setBackground.js
+++ src/view/styles/bodyImages/setBackground.js
@@ -7,7 +7,10 @@ export function setBackground()
     // Случайное число от 1 до 24
     const numberImg = Math.floor(Math.random() * 24) + 1;;
     const tegBody = document.querySelector('body');
+    tegBody.style.height = "100vh"; 
+    tegBody.style.margin = "0";
+    tegBody.style.backgroundSize = "cover";
+    tegBody.style.backgroundRepeat = "no-repeat";
+    tegBody.style.backgroundPosition = "center";
     tegBody.style.backgroundImage = `url('${path}background${numberImg}.jpg')`;
-    tegBody.style.backgroundSize = "cover";   // растянуть по экрану
-    tegBody.style.backgroundRepeat = "no-repeat"; // без повторов
 }
