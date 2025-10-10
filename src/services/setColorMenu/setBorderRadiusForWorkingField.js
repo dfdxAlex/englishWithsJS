1603e361f03d23a9bae92415751a660004379321
Dfdx Alex <alexmway78@gmail.com>
2025-10-10 19:04:52 +0200

    style for pole word1

--- src/services/setColorMenu/setBorderRadiusForWorkingField.js
+++ src/services/setColorMenu/setBorderRadiusForWorkingField.js
@@ -1,10 +1,17 @@
 import { returnBorderRadius } from './returnBorderRadius.js';
+import { generateColorPair } from '../../controllers/forHundleLevelX/generateColorPair.js';
+import { getContrastColor } from '../../controllers/forHundleLevelX/getContrastColor.js';
 
 export function setBorderRadiusForWorkingField(list)
 {
     list.forEach((el)=>{
         const selectEl = document.getElementById(el);
-        if (selectEl)
+        if (selectEl) {
             selectEl.style.borderRadius = returnBorderRadius(10, 150); 
+            selectEl.style.backgroundColor = generateColorPair();
+            selectEl.style.color = getContrastColor(generateColorPair());
+        }
     });
+
+
 }
