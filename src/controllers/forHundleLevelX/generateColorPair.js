bed4955a18509f43c3497b01380e176c2a89af98
Dfdx Alex <alexmway78@gmail.com>
2025-10-10 18:25:11 +0200

    add style for HandleLevelX

--- src/controllers/forHundleLevelX/generateColorPair.js
+++ src/controllers/forHundleLevelX/generateColorPair.js
@@ -13,20 +13,20 @@ export function generateColorPair() {
 
   // Если цвета используются средние, то для избежания схожести цвета фона и текста
   // Развести их по краям принудительно.
-  if ((rgb1[0] - rgb2[0]) < 50) {
-     rgb1[0] = getRandom(0, 100);
-     rgb2[0] = getRandom(150, 255);
-  }
+  // if ((rgb1[0] - rgb2[0]) < 128) {
+  //   //  rgb1[0] = getRandom(0, 100);
+  //    rgb2[0] = getRandom(150, 255);
+  // }
 
-  if ((rgb1[1] - rgb2[1]) < 50) {
-     rgb1[1] = getRandom(0, 100);
-     rgb2[1] = getRandom(150, 255);
-  }
+  // if ((rgb1[1] - rgb2[1]) < 50) {
+  //   //  rgb1[1] = getRandom(0, 100);
+  //    rgb2[1] = getRandom(150, 255);
+  // }
 
-  if ((rgb1[2] - rgb2[2]) < 50) {
-     rgb1[2] = getRandom(0, 100);
-     rgb2[2] = getRandom(150, 255);
-  }
+  // if ((rgb1[2] - rgb2[2]) < 50) {
+  //   //  rgb1[2] = getRandom(0, 100);
+  //    rgb2[2] = getRandom(150, 255);
+  // }
   // возвращаем оба формата
   return {
     color1: `rgb(${rgb1.join(', ')})`,
--- src/controllers/forHundleLevelX/setColorForLevelToAndThree.js
+++ src/controllers/forHundleLevelX/setColorForLevelToAndThree.js
@@ -7,14 +7,20 @@ export function setColorForLevelToAndThree()
     arrayButtonForLevelTwoAndThree().forEach((el)=>{
         const {color1, color2} = generateColorPair();
         el.style.backgroundColor = color1;
-        el.style.color = color2;
+        el.style.color = getContrastColor(color1);
     });
 
     // Отдельно разукрасть кнопку перевода и кнопку Проверить
     const buttonOk = document.getElementById('button-ok');
     buttonOk.style.backgroundColor = generateColorPair().color1;
-    buttonOk.style.color = generateColorPair().color2;
+    buttonOk.style.color = getContrastColor(generateColorPair().color1);
     const translate = document.getElementById('translate');
     translate.style.backgroundColor = generateColorPair().color1;
-    translate.style.color = generateColorPair().color2;
+    translate.style.color = getContrastColor(generateColorPair().color1);
+}
+
+function getContrastColor(rgb) {
+  const [r, g, b] = rgb.match(/\d+/g).map(Number);
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+  return brightness > 128 ? 'black' : 'white';
 }
