718934c00c9e52f60e8d9cbf8eb45cf0e3d10924
Dfdx Alex <alexmway78@gmail.com>
2025-10-10 18:58:12 +0200

    style for pole word2 and word3

--- src/controllers/forHundleLevelX/generateColorPair.js
+++ src/controllers/forHundleLevelX/generateColorPair.js
@@ -8,28 +8,6 @@ export function generateColorPair() {
     getRandom(0, 255),
   ];
 
-  // антипод — 255 минус каждый компонент
-  const rgb2 = rgb1.map(num => 255 - num);
-
-  // Если цвета используются средние, то для избежания схожести цвета фона и текста
-  // Развести их по краям принудительно.
-  // if ((rgb1[0] - rgb2[0]) < 128) {
-  //   //  rgb1[0] = getRandom(0, 100);
-  //    rgb2[0] = getRandom(150, 255);
-  // }
-
-  // if ((rgb1[1] - rgb2[1]) < 50) {
-  //   //  rgb1[1] = getRandom(0, 100);
-  //    rgb2[1] = getRandom(150, 255);
-  // }
-
-  // if ((rgb1[2] - rgb2[2]) < 50) {
-  //   //  rgb1[2] = getRandom(0, 100);
-  //    rgb2[2] = getRandom(150, 255);
-  // }
   // возвращаем оба формата
-  return {
-    color1: `rgb(${rgb1.join(', ')})`,
-    color2: `rgb(${rgb2.join(', ')})`
-  };
+  return `rgb(${rgb1.join(', ')})`;
 }
--- src/controllers/forHundleLevelX/setColorForLevelToAndThree.js
+++ src/controllers/forHundleLevelX/setColorForLevelToAndThree.js
@@ -2,21 +2,25 @@
 import { arrayButtonForLevelTwoAndThree } from './arrayButtonForLevelTwoAndThree.js';
 import { generateColorPair } from './generateColorPair.js';
 import { getContrastColor } from './getContrastColor.js';
+import { returnBorderRadius } from '../../services/setColorMenu/returnBorderRadius.js';
 
 export function setColorForLevelToAndThree()
 {
     arrayButtonForLevelTwoAndThree().forEach((el)=>{
-        const {color1, color2} = generateColorPair();
+        const color1 = generateColorPair();
         el.style.backgroundColor = color1;
         el.style.color = getContrastColor(color1);
     });
 
-    // Отдельно разукрасть кнопку перевода и кнопку Проверить
+    // Отдельно разукрасть кнопку Проверить
     const buttonOk = document.getElementById('button-ok');
-    buttonOk.style.backgroundColor = generateColorPair().color1;
-    buttonOk.style.color = getContrastColor(generateColorPair().color1);
+    buttonOk.style.backgroundColor = generateColorPair();
+    buttonOk.style.color = getContrastColor(generateColorPair());
+    buttonOk.style.borderRadius = returnBorderRadius(10, 150);  
+
+    // Отдельно разукрасть кнопку перевода
     const translate = document.getElementById('translate');
-    translate.style.backgroundColor = generateColorPair().color1;
-    translate.style.color = getContrastColor(generateColorPair().color1);
+    translate.style.backgroundColor = generateColorPair();
+    translate.style.color = getContrastColor(generateColorPair());
 }
 
