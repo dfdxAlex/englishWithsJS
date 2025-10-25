4e2440d17d3fa992994fad91e6a64e7b91fea9cb
Dfdx Alex <alexmway78@gmail.com>
2025-10-26 00:33:32 +0200

    Updates

--- js/controllers/handleButtonOk.js
+++ js/controllers/handleButtonOk.js
@@ -38,27 +38,10 @@ function handleButtonOk(ev)
     // Если ответ правильный
     if (testRezult) {
         window.handleOkErr('Ok',ev);
-        outputTranslateForTest();
+        window.outputTranslateForTest();
     } else {
         window.handleOkErr('Error',ev);
     }
 }
 
-function outputTranslateForTest()
-{
-    let commentPlus = '';
-    if (localStorage.getItem('randomNumber') !== "1") {
-        let transL = window.FactoryRegistr.getObject("LanguageController");
-        commentPlus = transL.translate('Речь примерно о:');
-        window.SettingForProgram.commentPlus.push(commentPlus);
-    } else {
-        window.SettingForProgram.commentPlus.push('');
-    }
 
-    window.SettingForProgram.dataElevator.push(indexQuestion()[5]);
-    const place = document.getElementById('clicked_element');
-    if (window.SettingForProgram.dataElevator.length > 1) {
-        commentPlus = window.SettingForProgram.commentPlus.shift();
-        place.innerText = commentPlus+' '+window.SettingForProgram.dataElevator.shift();
-    }
-}
