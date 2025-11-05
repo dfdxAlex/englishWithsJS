86f1ea21221f0f684af8bc3afaa0d0ac6e66b510
Dfdx Alex <alexmway78@gmail.com>
2025-11-05 22:51:04 +0100

    code rewiev

--- src/view/nav.js
+++ src/view/nav.js
@@ -13,7 +13,7 @@ export function nav()
     }, 0);
 
     document.getElementById('nav').innerHTML = `
-     <nav class="navbar navbar-expand-sm navbar-light" id="first-teg-nav-for-nav">
+     <nav class="navbar navbar-expand-md navbar-light" id="first-teg-nav-for-nav">
     <div class="container-fluid" id="nav-first-div">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"
