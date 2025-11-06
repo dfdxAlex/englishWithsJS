e1a6fb99ebca47d100e000243c9492f06ba3bd0e
Dfdx Alex <alexmway78@gmail.com>
2025-11-07 00:05:11 +0100

    code rewiev

--- src/view/nav.js
+++ src/view/nav.js
@@ -16,7 +16,7 @@ export function nav()
      <nav class="navbar navbar-expand-sm">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"
-              id="gamburger-menu-open"></span>
+              id="gamburger-menu-open">&#9776;</span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
