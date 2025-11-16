17512981c173dffedfccd96a1047e3d2862cb5ee
Dfdx Alex <alexmway78@gmail.com>
2025-11-16 23:53:10 +0100

    Updates

--- assets/index-CUx6cCJ4.js
+++ assets/index-CUx6cCJ4.js
@@ -3256,46 +3256,43 @@ var I=Object.defineProperty;var T=(e,a,t)=>a in e?I(e,a,{enumerable:!0,configura
       </div>
     </div>
   </div>
-</div>`}function returnLevel34(){return`<div class="card" style="width: 100%; border: 1px solid #28a745; border-radius: 5px; background-color: #e9f7ec;">
-  <div class="card-body">
-    <h4 class="card-title" style="font-size: 1.2rem; margin-bottom: 10px; color: #28a745;"><strong>Past Continuous Tense</strong></h4>
-    <p class="card-text" style="font-size: 1rem; color: #155724;">Past Continuous Tense описывает действия, которые происходили в определённый момент в прошлом или в течение определённого времени в прошлом. Он также используется для выражения двух действий, происходивших одновременно.</p>
+</div>`}function returnLevel34(){return`
+<div class="returnLevel34">
+    <h4><strong>Past Continuous Tense</strong></h4>
+    <p>Past Continuous Tense описывает действия, которые происходили в определённый момент в прошлом или в течение определённого времени в прошлом. Он также используется для выражения двух действий, происходивших одновременно.</p>
     <p><strong>Формула:</strong> [Subject] + was/were + [verb+ing]</p>
     
-    <h5 class="card-title" style="font-size: 1.1rem; margin-top: 15px; color: #28a745;">Примеры:</h5>
-    <div class="example" style="margin-bottom: 10px;">
-      <button class="btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#example1" aria-expanded="false" aria-controls="example1">
+    <h5>Примеры:</h5>
+    <ul>
+      <li>
         Пример 1
-      </button>
-      <div class="collapse" id="example1">
-        <p class="mt-2" style="color: #155724;">She was reading a book when I called her. (Она читала книгу, когда я позвонил ей.)</p>
-      </div>
-    </div>
-    <div class="example" style="margin-bottom: 10px;">
-      <button class="btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#example2" aria-expanded="false" aria-controls="example2">
+      </li>
+        <li>She was reading a book when I called her. (Она читала книгу, когда я позвонил ей.)</li>
+    </ul>
+
+    <ul>
+      <li>
         Пример 2
-      </button>
-      <div class="collapse" id="example2">
-        <p class="mt-2" style="color: #155724;">They were playing football all afternoon. (Они играли в футбол весь день.)</p>
-      </div>
-    </div>
-    <div class="example" style="margin-bottom: 10px;">
-      <button class="btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#example3" aria-expanded="false" aria-controls="example3">
+      </li>
+      <li>
+        They were playing football all afternoon. (Они играли в футбол весь день.)
+      </li>
+    </ul>
+
+    <ul>
+      <li>
         Пример 3
-      </button>
-      <div class="collapse" id="example3">
-        <p class="mt-2" style="color: #155724;">I was cooking dinner when the phone rang. (Я готовил ужин, когда зазвонил телефон.)</p>
-      </div>
-    </div>
-    <div class="example" style="margin-bottom: 10px;">
-      <button class="btn btn-success" type="button" data-bs-toggle="collapse" data-bs-target="#example4" aria-expanded="false" aria-controls="example4">
+      </li>
+      <li>I was cooking dinner when the phone rang. (Я готовил ужин, когда зазвонил телефон.)</li>
+    </ul>
+
+    <ul>
+      <li>
         Пример 4
-      </button>
-      <div class="collapse" id="example4">
-        <p class="mt-2" style="color: #155724;">We were talking about the movie when it started raining. (Мы говорили о фильме, когда начался дождь.)</p>
-      </div>
-    </div>
-  </div>
+      </li>
+        <li>We were talking about the movie when it started raining. (Мы говорили о фильме, когда начался дождь.)</li>
+    </ul>
+
 </div>`}function returnLevel34EN(){return`<div class="card" style="width: 100%; border: 1px solid #28a745; border-radius: 5px; background-color: #e9f7ec;">
   <div class="card-body">
     <h4 class="card-title" style="font-size: 1.2rem; margin-bottom: 10px; color: #28a745;"><strong>Past Continuous Tense</strong></h4>
