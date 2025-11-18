cfb63b016a33acf72fd1f1222eda78dfeadcea5e
Dfdx Alex <alexmway78@gmail.com>
2025-11-18 16:42:10 +0100

    Updates

--- assets/index-BLUNKTva.js
+++ assets/index-BLUNKTva.js
@@ -8765,7 +8765,9 @@ setPaddingForSimple() - устанавливает дополнительный 
         <label for="testType" class="form-label">Test Type</label>
         <select class="form-select" id="testType">
           <option value="simple" ${setSelectForSimple("simple")}>Simple Test</option>
+          <option value="simple-translate" ${setSelectForSimple("simple-translate")}>Simple Test Translate</option>
           <option value="word-assembly" ${setSelectForSimple("word-assembly")}>Word Assembly</option>
+          <option value="word-assembly-translate" ${setSelectForSimple("word-assembly-translate")}>Word Assembly Translate</option>
           <option value="word-assembly-not-translate" ${setSelectForSimple("word-assembly-not-translate")}>Word Assembly (not translate)</option>
         </select>
 
