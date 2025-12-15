8186f18e1645c7265e518b0bd81cd0852db13120
Dfdx Alex <alexmway78@gmail.com>
2025-12-16 00:28:25 +0100

    clean code

--- src/view/styles/bodyImages/setBackground.js
+++ src/view/styles/bodyImages/setBackground.js
@@ -36,35 +36,6 @@ tegBody.style.backgroundSize = "cover";
 tegBody.style.backgroundRepeat = "no-repeat";
 tegBody.style.backgroundPosition = "center center";
   
-// устанавливаем фон
-// tegBody.style.backgroundImage = `url('${imageUrl}')`;
-// tegBody.style.backgroundSize = "auto 100vh"; // по высоте: 100vh, по ширине — сколько нужно
-// tegBody.style.backgroundRepeat = "no-repeat";
-// tegBody.style.backgroundPosition = "center";
-
-// // слушаем движение мыши для смещения
-// document.addEventListener("mousemove", (e) => {
-//     const percent = e.clientX / window.innerWidth; // от 0 до 1
-//     tegBody.style.backgroundPosition = `${percent * 100}% center`;
-// });
-
-// tegBody.style.backgroundImage = `url('${imageUrl}')`;
-// tegBody.style.backgroundSize = "auto 100vh"; 
-// tegBody.style.backgroundRepeat = "no-repeat";
-// tegBody.style.backgroundPosition = "0% center";
-
-// let pos = 0;          // начальная позиция
-// let direction = 0.1;    // 1 — вправо, -1 — влево
-
-// // авто-анимация
-// setInterval(() => {
-//     pos += direction * 1; // скорость (0.1 = медленно, можно увеличить)
-
-//     if (pos >= 100) direction = -1; // дошли до правого края — обратно
-//     if (pos <= 0) direction = 1;    // дошли до левого края — вперед
-
-//     tegBody.style.backgroundPosition = `${pos}% center`;
-// }, 20);
 
     setColorMenu();
 
