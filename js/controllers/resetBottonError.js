8fa71e3eb170148e184d2565492191bc8ead6df4
Dfdx Alex <alexmway78@gmail.com>
2025-12-27 12:31:33 +0100

    Updates

--- js/controllers/resetBottonError.js
+++
@@ -1,8 +0,0 @@
-function resetBottonError()
-{
-        // Достать из регистра объект переводчика
-        const translate = window.LanguageController;
-
-        // Сброс состояния кнопки
-        document.getElementById('search_error').textContent = translate.translate('Сообщить об ошибке');
-}
