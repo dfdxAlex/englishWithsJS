985a0b9265c40cf511741bfd0f0d1bb037f01dab
Dfdx Alex <alexmway78@gmail.com>
2026-01-23 08:53:39 +0100

    code rewiev

--- src/models/filedb/extendForArray/searchIndex234.js
+++ src/models/filedb/extendForArray/searchIndex234.js
@@ -1,53 +1,62 @@
 import { getRandomInt } from '../../../services/getRandomInt.js';
-import { is_notWord } from '../../../view/WorkingField/is_notWord.js';
+// import { is_notWord } from '../../../view/WorkingField/is_notWord.js';
 import { detectPlaceholder } from '../../../view/WorkingField/detectPlaceholder.js';
 
 export function searchIndex234(arrayLocal, index, timeArray, marker)
 {
-                // Сгенерировать номер случайного подмассива
-                let randomeNomberArray = getRandomInt(0, arrayLocal.length-1);
+                // получить случайный подмассив
+                const arrayRandome = getRandomeArray(arrayLocal);
 
                 // если нулевой элемент случайного подмассива с маркером, то поместить
                 // эту строку в позицию index рабочего подмассива
-                if (arrayLocal[randomeNomberArray][0].includes(marker)) {
-                    let arrayOne = arrayLocal[randomeNomberArray][0];
+                // Маркером может быть знак ? - значит работаем с вопросами ...
+                if (arrayRandome[0].includes(marker)) {
+
                     // если в случайно выбранное предложение попало предложение с пропущенным словом
                     // то вставить его.
-                    if (is_notWord(arrayLocal[randomeNomberArray])) {
-                        arrayOne.replace(detectPlaceholder(arrayLocal[randomeNomberArray]),arrayLocal[randomeNomberArray][1]);
-                    }
-                    timeArray[index] = arrayOne;
+                    timeArray[index] = arrayRandome[0].replace(detectPlaceholder(arrayRandome),arrayRandome[1]);
+
                 } else {
                     // иначе ищем вопросительные знаки в остальной части подмассива случайного
                     // придумать случайное число от 1 до числа правильных ответов в подмассиве
                     let randomeNomberEl = getRandomInt(1, arrayLocal.lengthTrue-1);
 
                     // если нашли предложение с вопросительным, то помещаем его в индекс 2
-                    if (arrayLocal[randomeNomberArray][randomeNomberEl].includes(marker)) {
-                        let arrayOne = arrayLocal[randomeNomberArray][randomeNomberEl];
+                    if (arrayRandome[randomeNomberEl].includes(marker)) {
                         // если в случайно выбранное предложение попало предложение с пропущенным словом
                         // то вставить его.
-                        if (is_notWord(arrayLocal[randomeNomberArray]) && randomeNomberEl === 0) {
-                            arrayOne.replace(detectPlaceholder(arrayLocal[randomeNomberArray]),arrayLocal[randomeNomberArray][1]);
-                        }
-                        timeArray[index] = arrayOne;
+                        timeArray[index] = arrayRandome[randomeNomberEl].replace(detectPlaceholder(arrayRandome),arrayRandome[1]);
                         
                     } else {
+
                         let loop = false;
+                        // Выбрать номер слуайного подмассива из всего массива
+                        let randomeNomberArray = getRandomInt(0, arrayLocal.length-1);
+
                         for(let i = randomeNomberArray; i < arrayLocal.length; i++) {
-                            randomeNomberEl = getRandomInt(1, arrayLocal.lengthTrue - 1);
-                            // Если работаем с минитекстом, то всегда просматривать только в нулевом элементе
-                            if (marker === '. ') {
-                                randomeNomberEl = 0;
-                                i += 7;
-                                
+
+                            let randomeNomberEl = 0;
+                            
+                            // Если не работаем с минитекстом, то можно просматривать случайный элемент
+                            if (marker !== '. ') {
+                                randomeNomberEl = getRandomInt(1, arrayLocal.lengthTrue - 1);
                             }
+
                             // Если вышли за пределы массива то вернуться обратно 
                             if (arrayLocal.length <= i) i = arrayLocal.length - 1;
+
+                            // здесь выбранный случайно подмассив проверяется на маркер и 
+                            // если маркер есть, то взять предложение выбранного из 
+                            // подмассива
                             if (arrayLocal[i][randomeNomberEl].includes(marker)) {
                                 timeArray[index] = arrayLocal[i][randomeNomberEl];
+                                timeArray[index] = timeArray[index].replace(detectPlaceholder(arrayLocal[i]),arrayLocal[i][1]);
                                 break;
                             }
+
+                            // Дальше есть проверка, если добежали до конца массива от случайно-выбранного элемента
+                            // и не нашли нужное число предложений, то начать с нулевого индекса
+                            // такая акция происходит только однажды, потому что второй раз loop уже будет true
                             if (i === arrayLocal.length-2 && !loop) {
                                 i=0;
                                 loop = true;
@@ -58,4 +67,12 @@ export function searchIndex234(arrayLocal, index, timeArray, marker)
                         }
                     }
                 }
+}
+
+
+function getRandomeArray(arrayR)
+{
+    const arrayTime = arrayR;
+    // Сгенерировать номер случайного подмассива
+    return arrayTime[getRandomInt(0, arrayTime.length-1)];
 }
